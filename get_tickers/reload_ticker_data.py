40db6b2eda8498957553d3192eee54d7ef9d7211
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-06 13:01:51 +0000

    parquet data/documentation/customisation for search of tickers
    removed data from gitignore
    added documentation to current functions
    improved search for tickers and personal customisation for downloads

--- get_tickers/reload_ticker_data.py
+++ get_tickers/reload_ticker_data.py
@@ -2,26 +2,51 @@ import pandas as pd
 import pandas_datareader.data as web
 # !pip install yfinance==0.1.62
 import yfinance as yf
-def save_indexes(interval='1d',sp500=True,ftse100=True):
+def save_indexes(interval:str='1d',sp500:bool=True,ftse100:bool=True)->None:
+    '''
+    Use hardcoded tickers for indexes to retrieve data in timeseries format from yahoo finance
+    Currently available for SP500 & FTSE100
+    interval available values 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+    Period is max as default behavior
+    Saves data in the specified paths
+    '''
     yf.pdr_override()
 
     path = 'Ticker Data//Indexes'
     if sp500:
         print('Downloading SP500')
-        df = web.DataReader('^GSPC',interval=interval)
-        df.to_parquet(f'{path}//SP500.parquet')
+        df = web.DataReader('^GSPC',interval=interval,progress=False)
+        if len(df)==0:
+            print('SP500 failed to download')
+        else:
+            df.to_parquet(f'{path}//SP500.parquet')
     if ftse100:
         print('Downloading FTSE100')
-        df = web.DataReader('^FTSE',interval=interval)
-        df.to_parquet(f'{path}//FTSE100.parquet')
+        df = web.DataReader('^FTSE',interval=interval,progress=False)
+        if len(df)==0:
+            print('FTSE100 failed to download')
+        else:
+            df.to_parquet(f'{path}//FTSE100.parquet')
 
 
-def save_tickers(tickers,interval='1d'):
+def save_tickers(tickers:list,interval:str='1d',suffix=None)->None:
+    '''
+    Use given tickers to retrieve data in timeseries format from yahoo finance.
+    If ticker is not available it will print an appropriate message to the console
+    interval available values 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+    Period is max as default behavior
+    Saves data in the specified paths
+    '''
     path = 'Ticker Data//Tickers'
-    for ticker in tickers:
-        print(f'Downloading {ticker}')
-        df = web.DataReader(ticker,interval=interval)
-        if len(df)>0:
+    for idx,ticker in enumerate(tickers):
+        print(f'{idx+1}/{len(tickers)}')
+        df = web.DataReader(ticker,interval=interval,progress=False)
+        if len(df)==0:
+            if suffix:
+                df = web.DataReader(ticker+suffix,interval=interval,progress=False)
+        if len(df)==0:
+            print(f'{ticker} failed to download')
+        else:
             df.to_parquet(f'{path}//{ticker}.parquet')
 
         
--- get_tickers/reload_tickers.py
+++ get_tickers/reload_tickers.py
@@ -1,15 +1,25 @@
 import requests
 import pandas as pd
 
-def reload_SP500_tickers():
-    url="https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    df = pd.read_html(url)[0]
-    df = df[['Symbol','Security','GICS Sector']]
-    df.columns = ['Ticker','Security','Sector']
-    return df
+def reload_SP500_tickers()->pd.DataFrame:
+	'''
+	Use wiki URL to retrieve current SP500 tickers
+	Returns dataframe that contains the tickers, the name of the companies and the Sector the companies operate in
+	If wiki page changes this needs to be revisited
+	'''
+	url="https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+	df = pd.read_html(url)[0]
+	df = df[['Symbol','Security','GICS Sector']]
+	df.columns = ['Ticker','Security','Sector']
+	return df
 
-def reload_FTSE100_tickers():
-    url = 'https://en.wikipedia.org/wiki/FTSE_100_Index'
-    df = pd.read_html(url)[4]
-    df.columns = ['Security','Ticker','Sector']
-    return df[['Ticker','Security','Sector']]
+def reload_FTSE100_tickers()->pd.DataFrame:
+	'''
+	Use wiki URL to retrieve current FTSE100 tickers
+	Returns dataframe that contains the tickers, the name of the companies and the Sector the companies operate in
+	If wiki page changes this needs to be revisited
+	'''
+	url = 'https://en.wikipedia.org/wiki/FTSE_100_Index'
+	df = pd.read_html(url)[4]
+	df.columns = ['Security','Ticker','Sector']
+	return df[['Ticker','Security','Sector']]
