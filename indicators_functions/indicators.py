ed83382cbb11590f951e3da2571feefb3c7243f1
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-15 19:21:24 +0000

    indicators updating

--- indicators_functions/indicators.py
+++ indicators_functions/indicators.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
-def SMA(periods=14):
+def SMA(df,periods=14):
 	return df.rolling(window=periods).mean()
+
+def EMA(df,periods=14):
+	return df.ewm(span=periods,min_periods=periods).mean()
+
+def ATR(df,span=14):
+	high_low = df['High'] - df['Low']
+    high_close = np.abs(df['High'] - df['Close'].shift())
+    low_close = np.abs(df['Low'] - df['Close'].shift())
+    ranges = pd.concat([high_low, high_close, low_close], axis=1)
+    true_range = np.max(ranges, axis=1)
+    return true_range.rolling(span).sum()/span
